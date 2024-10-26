9095fed553bb1ed8b49898f9635809d88ba8728e
David Lafontant <lafontant.david@gmail.com>
2024-10-26 19:38:16 -0400

    Ccreate stinulus controller to trigger the offcanvas menu

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import OffcanvasNavbarController from "./offcanvas_navbar_controller"
+application.register("offcanvas-navbar", OffcanvasNavbarController)
