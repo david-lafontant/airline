7134b80eadad66f8d9e47cba3bdba4103317fc9e
David Lafontant <lafontant.david@gmail.com>
2024-10-26 16:54:13 -0400

    Create root path and alias for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'publics/home'
-  get 'publics/contact'
+  root 'publics#home'
+  get 'contact', to:'publics#contact', as: :contact
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
