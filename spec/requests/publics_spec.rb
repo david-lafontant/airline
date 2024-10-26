94bd1a60b982c70daa6b77b68528b76ec8abab06
David Lafontant <lafontant.david@gmail.com>
2024-10-26 16:53:35 -0400

    Customize test to root  and contact alias routes

--- spec/requests/publics_spec.rb
+++ spec/requests/publics_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Publics", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/publics/home"
+RSpec.describe 'Publics', type: :request do
+  describe 'GET /' do
+    it 'returns http success' do
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /contact" do
-    it "returns http success" do
-      get "/publics/contact"
+  describe 'GET /contact' do
+    it 'returns http success' do
+      get '/contact'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
